a039214c37f09e0d200b90c07071c73197ea66ab
vip529 <srvip028@gmail.com>
2020-11-15 19:42:10 +0530

    s2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const port = 8080
 app.use(express.urlencoded());
 let studentArray = require("./InitialData");
-
+let studentCount = studentArray.length;
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,7 +20,7 @@ app.get('/api/student/:id',(req,res)=>{
     const id = req.params.id;
     const student = studentArray.filter((student)=>student.id === Number(id));
     if(student.length !== 0){
-        res.json(student);
+        res.json(student[0]);
     }else{
         res.sendStatus(404);
         return;
@@ -33,17 +33,18 @@ app.post('/api/student',(req,res)=>{
     const data = req.body;
     const {name,currentClass,division} = req.body;
     if(data && name && currentClass && division){
-        const lastId = studentArray.length !== 0 ? studentArray[studentArray.length -1].id : -1;
+        // const lastId = studentArray.length !== 0 ? studentArray[studentArray.length -1].id : -1;
+        studentCount++;
         const newStudent = {
-            id:lastId+1,
+            id:studentCount,
             name:name,
             currentClass:currentClass,
             division:division,
 
         }
         studentArray.push(newStudent);
-        res.set('content-type','application/x-www-form-urlencoded');
-        res.json(lastId+1);
+        // res.set('content-type','application/x-www-form-urlencoded');
+        res.send(studentCount);
     }else{
         res.sendStatus(400);
         return;
@@ -84,8 +85,9 @@ app.put('/api/student/:id',(req,res)=>{
                 })
                 if(!idFound){
                     if(Object.keys(data).length === 3){
-                        const lastId = studentArray.length !== 0 ? studentArray[studentArray.length -1].id : -1;
+                        // const lastId = studentArray.length !== 0 ? studentArray[studentArray.length -1].id : -1;
                         const {name,currentClass,division} = data;
+                        studentCount++;
                         const newStudent = {
                             id:lastId+1,
                             name:name,
@@ -96,7 +98,7 @@ app.put('/api/student/:id',(req,res)=>{
 
                         studentArray.push(newStudent);
                         res.set('content-type','application/x-www-form-urlencoded')
-                        res.json(studentArray)
+                        // res.json(studentArray)
                     }else{
                         res.sendStatus(400)
                     }
@@ -104,7 +106,7 @@ app.put('/api/student/:id',(req,res)=>{
                     studentArray = []
                     studentArray = [...newStudents]
                     res.set('content-type','application/x-www-form-urlencoded')
-                    res.json(studentArray);
+                    // res.json(studentArray);
                 }
                 
             }
@@ -126,7 +128,7 @@ app.delete('/api/student/:id',(req,res)=>{
         studentArray = [...filteredStudent]
         res.json('deleted');
     }else{
-        res.sendStatus(400)
+        res.sendStatus(404)
     }
 
 })
